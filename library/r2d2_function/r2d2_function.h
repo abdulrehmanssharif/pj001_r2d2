59302c88e1a7623d131238369c726395bab175bf
Abdul-Rehman Sharif <ars_alisharif@hotmail.com>
2015-12-26 14:45:29 +0000

    Removed the added sub repositories - will be adding the libraries directly in next commit.

--- library/r2d2_function/r2d2_function.h
+++ library/r2d2_function/r2d2_function.h
@@ -1,36 +1,61 @@
 // Header file for functions.cpp
+// Copyright 2014/2015
+// Authors: Alessandro Grande, Abdul-Rehman Sharif, Lucinda Hartfield
+
+// This also contains Ardumoto.h library functions to control DC motors\
+// Copyright 2012 Jeroen Doggen (jeroendoggen@gmail.com)
 
 #ifndef R2D2_FUNCTION
 #define R2D2_FUNCTION
 
+// Class specifies most of the functions needed for proj001_r2d2
 class r2d2_function {
   public:
-    // Global parameters
-    // Speed at which it move_forwards an object
-    // optimum value: 0.4 to 0.8
-    extern float moveforwardspeed;
-    // Speed at which it rotates to find an object
-    // optimum value: 0.3 to 0.5
-    extern float searchspeed;
-    // Range of detection
-    // optimum value: 30 to 50
-    extern unsigned int range;
+    // Constructor
+    r2d2_function();
+    // Deconstructor
+    ~r2d2_function();
 
-    void flash_leds();
-    void turn_leds_on();
-    void turn_led_right();
-    void turn_led_left();
-    int read_line1();
-    int read_line2();
+    void begin();
+    void beginMotoA(int directionPin, int pwmPin);
+    void beginMotoB(int directionPin, int pwmPin);
+
+    // Read line sensor left
+    int read_lline();
+    // Read line sensor right
+    int read_rline();
+    // Returns 1 (front), -1 (back) and 0 (no) if line is detected
     int detect_line();
-    void move_random(float speed=moveforwardspeed);
-    void reverse(float speed = moveforwardspeed);
-    void turn(float speed = moveforwardspeed);
-    void reverse_and_turn(float speed = moveforwardspeed);
-    void move_forward(float speed = moveforwardspeed);
+    // Results in random movement of bot
+    void move_random(float speed);
+    // Reverses the bot
+    void reverse(float speed);
+    // Turns the bot - 360 cw motion
+    void turn(float speed);
+    // Reverse and turns the bot
+    void reverse_and_turn(float speed);
+    // Drives the bot forward
+    void drive(float speed);
+    // Stops the bot
     void stop();
-    int detect_object(int range_t = range, float speed = moveforwardspeed);
-    void move_detect(float speed, int fwd_bck, int time, int wait=0);
+    // Slowly brakes only one of the specified motors
+    void brake(char moto);
+    // Immediately breakes one of the specified motors
+    void brakeAgressive(char moto);
+    // Detects an object using ultra sonic sensors
+    int detect_object(int range, float speed);
+    // Detects and object while moving in a 360 rotation
+    void move_detect(float speed, int fwd_bck, int time, int wait);
+
+  private:
+    // Pin allocation
+    // Ultra sonic sensor
+    int trig_pin, echo_pin;
+    // Line sensor
+    int lleft_pin, lright_pin;
+    // Motor
+    int in1_mleft_pin, in2_mleft_pin, en_mleft_pin;
+    int in1_mright_pin, in2_mright_pin, en_mright_pin;
 };
 
 #endif
--- r2d2_main/r2d2_main.ino
+++ r2d2_main/r2d2_main.ino
@@ -3,6 +3,18 @@
 #include <Ardumoto.h>
 #include <defines.h>
 
+// Global variables - used when any functions are called
+// Variables to set speed and range
+// Speed at which it move_forwards an object
+// optimum value: 0.4 to 0.8
+float drive_speed;
+// Speed at which it rotates to find an object
+// optimum value: 0.3 to 0.5<D-d>
+float search_speed;
+// Range of detection
+// optimum value: 30 to 50
+unsigned int range;
+
 void setup() {
 
 }
